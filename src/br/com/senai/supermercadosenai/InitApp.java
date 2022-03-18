fb666a9b1e80c3ecb74f1a0a3e65cc343a040a74
pdr98 <pedrosnider98@gmail.com>
2022-03-18 20:11:05 -0300

    Primeiro commit do projeto

---
+++ src/br/com/senai/supermercadosenai/InitApp.java
@@ -0,0 +1,11 @@
+package br.com.senai.supermercadosenai;
+
+public class InitApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Iniciando servidor...");
+
+    }
+
+}
